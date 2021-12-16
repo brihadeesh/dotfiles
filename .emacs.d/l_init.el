896c26505ce368c3765f2e2c8fbaeef804d2aae5
brihadeesh <brihadeesh.santharam@gmail.com>
2021-12-16 14:12:31 +0530

    org-code block bulk commenting works well

--- .emacs.d/l_init.el
+++ .emacs.d/l_init.el
@@ -1,1 +1,5 @@
 ;; -*- lexical-binding: t -*-
+
+;; don't follow symlinks? hopefully this solves the
+;; `symbols function definition is void: org-file-name-concat' error
+(setq vc-follow-symlinks nil)
